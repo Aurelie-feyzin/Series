0f4918db2ce25a44f2512fad554a4dec0a45be89
Aurelie <gramusset.a@gmail.com>
2020-03-16 20:58:43 +0100

    Actor génération du CRUD

---
+++ src/Form/ActorType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Actor;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ActorType extends AbstractType
+{
+    /**
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstname')
+            ->add('lastname')
+            ->add('createdAt')
+            ->add('updatedAt')
+            ->add('programs')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Actor::class,
+        ]);
+    }
+}
