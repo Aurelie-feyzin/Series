87e3a108cba895b03dcc71641880e63fc4865c98
Aurelie <gramusset.a@gmail.com>
2020-03-19 13:11:40 +0100

    Modification fixture pour avoir plus de program dans moins de categorie

--- fixtures/episode.php
+++ fixtures/episode.php
@@ -1,13 +1,14 @@
 <?php declare(strict_types=1);
 
 $episodes = [];
-$nbFixtures = 300;
+$nbFixtures = 500;
+$nbSeason = 100;
 for ($i = 1; $i <= $nbFixtures; ++$i) {
     $program = '$program_1';
     $episodes['App\Entity\Episode']['episode' . $i] = [
-        'season'   => '@season_' . (($i % 30) + 1),
+        'season'   => '@season_' . (($i % $nbSeason) + 1),
         'title'    => '<sentence(5, true)>',
-        'number'   => (int) ($i / 30) + 1,
+        'number'   => (int) ($i / $nbSeason) + 1,
         'synopsis' => '60%? <text(255)>',
     ];
 }
--- fixtures/season.php
+++ fixtures/season.php
@@ -1,12 +1,13 @@
 <?php declare(strict_types=1);
 
 $seasons = [];
-$nbFixtures = 30;
+$nbFixtures = 100;
+$nbPrograms = 25;
 for ($i = 1; $i <= $nbFixtures; ++$i) {
     $program = '$program_1';
     $seasons['App\Entity\Season']['season_' . $i] = [
-        'program'     => '@program_' . (($i % 10) + 1),
-        'number'      => (int) ($i / 10) + 1,
+        'program'     => '@program_' . (($i % $nbPrograms) + 1),
+        'number'      => (int) ($i / $nbPrograms) + 1,
         'year'        => '40%? <numberBetween(1990, 2021)>',
         'description' => '60%? <text(255)>',
     ];
