e59d7767d7adfbb1a7aebb7b84be9e8a842fcda9
Aurelie <gramusset.a@gmail.com>
2020-03-19 16:53:45 +0100

    Season : CRUD auto-générer

---
+++ src/Form/SeasonType.php
@@ -0,0 +1,32 @@
+<?php declare(strict_types=1);
+
+namespace App\Form;
+
+use App\Entity\Season;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SeasonType extends AbstractType
+{
+    /**
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('number')
+            ->add('year')
+            ->add('description')
+            ->add('createdAt')
+            ->add('updatedAt')
+            ->add('program');
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Season::class,
+        ]);
+    }
+}
