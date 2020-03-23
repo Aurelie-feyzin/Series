ddbed3ebb82fbc1de86ac3bfee8bc16dad03d330
Aurelie <gramusset.a@gmail.com>
2020-03-23 18:07:18 +0100

    Comment : formulaire d'edition et de création fonctionnel

--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Comment;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,12 +17,14 @@ class CommentType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('comment')
-            ->add('rate')
-            ->add('createdAt')
-            ->add('updatedAt')
-            ->add('episode')
-            ->add('author');
+            ->add('comment', TextareaType::class)
+            ->add('rate', IntegerType::class, [
+                'attr' => [
+                    'min' => 0,
+                    'max' => 5,
+                ],
+                'help' => 'Nombre entier comprit entre 0 et 5',
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
