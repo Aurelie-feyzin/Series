b3f777136f3afdfa56cc2c5463054e828eeee01e
Aurelie <gramusset.a@gmail.com>
2020-04-01 18:57:04 +0200

    Fixtures : augmentation - Actor - Comment (modification de Episode pour avoir 250 commentaires d'un Episode) - Program - User

--- fixtures/comment.php
+++ fixtures/comment.php
@@ -3,8 +3,8 @@
 $comments = [];
 
 // Fixture pour user user@email.fr
-$nbFixturesFix = 10;
-for ($i = 1; $i <= $nbFixturesFix; ++$i) {
+$nbFixturesUser = 250;
+for ($i = 1; $i <= $nbFixturesUser; ++$i) {
     $comments['App\Entity\Comment']['comment_' . $i] = [
         '__construct' => [
             'episode' => '@episode_' . $i,
@@ -14,11 +14,26 @@ for ($i = 1; $i <= $nbFixturesFix; ++$i) {
         'rate'    => '<numberBetween(0, 5)>',
     ];
 }
+
+// Fixture pour episode 1
+$nbFixturesComment = 250;
+$nbUser = 500;
+$nbFixturesFixed = $nbFixturesUser + $nbFixturesComment;
+for ($i = $nbFixturesUser; $i <= $nbFixturesFixed; ++$i) {
+    $comments['App\Entity\Comment']['comment_' . $i] = [
+        '__construct' => [
+            'episode' => '@episodeComment',
+            'author'  => '@user_' . (($i % $nbUser) + 1),
+        ],
+        'comment' => '<text(255)>',
+        'rate'    => '<numberBetween(0, 5)>',
+    ];
+}
+
 // Random Fixtures
-$nbUser = 25;
-$nbFixturesRandom = 50;
-$nbFixturesTot = $nbFixturesFix + $nbFixturesRandom;
-for ($i = $nbFixturesFix; $i <= $nbFixturesTot; ++$i) {
+$nbFixturesRandom = $nbUser;
+$nbFixturesTot = $nbFixturesFixed + $nbFixturesRandom;
+for ($i = $nbFixturesFixed; $i <= $nbFixturesTot; ++$i) {
     $comments['App\Entity\Comment']['comment_' . $i] = [
         '__construct' => [
             'episode' => '@episode_*',
--- fixtures/episode.php
+++ fixtures/episode.php
@@ -3,6 +3,14 @@
 $episodes = [];
 $nbFixtures = 500;
 $nbSeason = 100;
+
+$episodes['App\Entity\Episode']['episodeComment'] = [
+    'season'   => '@season_1',
+    'title'    => '<sentence(5, true)>',
+    'number'   => 1,
+    'synopsis' => '60%? <text(255)>',
+];
+
 for ($i = 1; $i <= $nbFixtures; ++$i) {
     $episodes['App\Entity\Episode']['episode_' . $i] = [
         'season'   => '@season_' . (($i % $nbSeason) + 1),
