6b026a3f8a089e4a4705b66261b087daaa4b8917
Aurelie <gramusset.a@gmail.com>
2020-04-04 20:11:41 +0200

    i18n configuration

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,11 +3,23 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+    /**
+     * @Route("/", name="homepage", methods={"GET"})
+     */
+    public function homepage(Request $request): Response
+    {
+        $locale = $request->getLocale();
+        $request->getSession()->set('_locale', $locale);
+
+        return $this->redirectToRoute('index');
+    }
+
     /**
      * @Route("/", name="index", methods={"GET"})
      */
