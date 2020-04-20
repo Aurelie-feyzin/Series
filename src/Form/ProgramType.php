efb0d2d9b834bdf936f61577adda3ff0b4398c37
Aurelie <gramusset.a@gmail.com>
2020-04-20 10:41:44 +0200

    Program template _form i18n

--- src/Form/ProgramType.php
+++ src/Form/ProgramType.php
@@ -7,6 +7,9 @@ use App\Entity\Country;
 use App\Entity\Program;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\PropertyAccess\PropertyPath;
@@ -20,11 +23,25 @@ class ProgramType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('synopsis')
-            ->add('year')
-            ->add('slug')
+            ->add('title', TextType::class, [
+                'required' => true,
+            ])
+            ->add(
+                'synopsis',
+                TextareaType::class,
+                [
+                    'required' => false,
+                ]
+            )
+            ->add('year', IntegerType::class, [
+                'required' => false,
+                'attr'     => [
+                    'min' => 1890,
+                    'max' => 2500,
+                ],
+            ])
             ->add('country', EntityType::class, [
+                'required'     => false,
                 'class'        => Country::class,
                 'choice_label' => 'name',
                 'expanded'     => false,
